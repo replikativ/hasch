6ef118b0d9d691be83b1f5108a902502873254eb
Christian Weilbach <christian_weilbach@web.de>
2015-10-05 11:41:24 +0200

    Use fixed incognito version.

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/clojurescript "1.7.48"]
-                 [io.replikativ/incognito "0.1.1"]]
+                 [io.replikativ/incognito "0.1.2"]]
   :source-paths ["src"]
   :plugins [[lein-cljsbuild "1.0.6"]]
 
