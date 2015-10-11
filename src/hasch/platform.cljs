31ed0e223ad440f6d4009f6174e08715ce22707e
Christian Weilbach <christian_weilbach@web.de>
2015-10-11 23:43:08 +0200

    Use provided cljs random uuid.

--- src/hasch/platform.cljs
+++ src/hasch/platform.cljs
@@ -12,26 +12,7 @@
                          (cemerick.austin/repl-env)))
     (cemerick.austin.repls/cljs-repl repl-env))
 
-;; taken from https://github.com/whodidthis/cljs-uuid-utils/blob/master/src/cljs_uuid_utils.cljs
-;; Copyright (C) 2012 Frank Siebenlist
-;; Distributed under the Eclipse Public License, the same as Clojure.
-(defn uuid4
-  "(uuid4) => new-uuid
-   Arguments and Values:
-   new-uuid --- new type 4 (pseudo randomly generated) cljs.core/UUID instance.
-   Description:
-   Returns pseudo randomly generated UUID,
-   like: xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx as per http://www.ietf.org/rfc/rfc4122.txt.
-   Examples:
-   (uuid4) => #uuid \"305e764d-b451-47ae-a90d-5db782ac1f2e\"
-   (type (uuid4)) => cljs.core/UUID"
-  []
-  (letfn [(f [] (.toString (rand-int 16) 16))
-          (g [] (.toString (bit-or 0x8 (bit-and 0x3 (rand-int 15))) 16))]
-    (UUID. (.toString (.append (goog.string.StringBuffer.)
-       (f) (f) (f) (f) (f) (f) (f) (f) "-" (f) (f) (f) (f)
-       "-4" (f) (f) (f) "-" (g) (f) (f) (f) "-"
-       (f) (f) (f) (f) (f) (f) (f) (f) (f) (f) (f) (f))))))
+(def uuid4 random-uuid)
 
 (defn byte->hex [b]
   (-> b
