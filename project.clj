b5aa422d66cf413e41825d7cdcc72e766ade17e1
James Conroy-Finn <git@invetica.co.uk>
2016-12-07 17:32:04 +0000

    Fix project URL

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject io.replikativ/hasch "0.3.0"
   :description "Cryptographic hashing of EDN datastructures."
-  :url "http://github.com/ghubber/hasch"
+  :url "http://github.com/replikativ/hasch"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
