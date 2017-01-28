390a3e8fb57fe417667d3d0e1210905e0bf7443b
Christian Weilbach <christian@topiq.es>
2017-01-28 16:11:33 +0100

    Add figwheel REPL, support cljs tests.

--- project.clj
+++ project.clj
@@ -10,8 +10,16 @@
   :plugins [[lein-cljsbuild "1.1.4"]]
 
   :aliases {"all" ["with-profile" "default:+1.7:+1.8"]}
-  :profiles {:dev {:dependencies [[criterium "0.4.4"]]
-                   :plugins [[com.cemerick/austin "0.1.6"]]}
+  :profiles {:dev {:dependencies [[figwheel-sidecar "0.5.8"]
+                                  [com.cemerick/piggieback "0.2.1"]
+                                  [criterium "0.4.4"]]
+                   :figwheel {:nrepl-port 7888
+                              :nrepl-middleware ["cider.nrepl/cider-middleware"
+                                                 "cemerick.piggieback/wrap-cljs-repl"]}
+                   :plugins [[lein-figwheel "0.5.8"]]
+                   :repl-options {; for nREPL dev you really need to limit output
+                                  :init (set! *print-length* 50)
+                                  :nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}}
              :1.7 {:dependencies [[org.clojure/clojure "1.7.0"]
                                   [org.clojure/clojurescript "1.7.228"]]}
              :1.8 {:dependencies [[org.clojure/clojure "1.8.0"]
@@ -21,13 +29,27 @@
 
   :cljsbuild
   {:builds
-   {:main {:source-paths ["src"]
-           :compiler
-           {:output-to "resources/public/js/main.js"
-            :optimizations :advanced}}
-    :test {:source-paths ["src"]
-           :compiler
-           {:output-to "target/cljs/testable.js"
-            :optimizations :whitespace
-            :pretty-print true}}}
-   :test-commands {"unit-tests" ["phantomjs" "target/cljs/testable.js"]}})
+   [{:id "cljs_repl"
+     :source-paths ["src"]
+     :figwheel true
+     :compiler
+     {:main hasch.core
+      :asset-path "js/out"
+      :output-to "resources/public/js/client.js"
+      :output-dir "resources/public/js/out"
+      :optimizations :none
+      :pretty-print true}}
+    #_{:id "main"
+     :source-paths ["src"]
+     :compiler
+     {:output-to "resources/public/js/main.js"
+      :optimizations :advanced}}
+    {:id "test"
+     :source-paths ["src" "test"]
+     :compiler
+     {:output-to "resources/private/js/unit-test.js"
+      :output-dir "resources/private/js/out"
+      :optimizations :whitespace
+      :pretty-print true}}]
+   :test-commands {"unit-tests" ["phantomjs" "resources/private/js/test.js"
+                                 "resources/private/html/unit-test.html"]}})
--- test/hasch/test.cljc
+++ test/hasch/test.cljc
@@ -104,6 +104,18 @@
 
 #_(run-tests)
 
+#?(:cljs
+   (deftest utf8-test
+     (is (= (utf8 "小鳩ちゃんかわいいなぁ")
+            #js [229 176 143 233 179 169 227 129 161 227 130 131 227 130 147 227
+                 129 139 227 130 143 227 129 132 227 129 132 227 129 170 227 129 129]))))
+
+#?(:cljs
+   (defn ^:export run
+     []
+     (enable-console-print!)
+     (run-tests)
+     #_(run-all-tests #_"hasch.*-test")))
 
 ;; fire up repl
 #_(do
