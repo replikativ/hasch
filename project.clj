86055a34f2df35c70d55fe648b101f6a534d71b0
Wade Dominic <wade@lambdaforge.io>
2020-08-31 21:43:21 +0200

    Bump dependencies

--- project.clj
+++ project.clj
@@ -4,8 +4,8 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.10.1" :scope "provided"]
-                 [org.clojure/clojurescript "1.10.597" :scope "provided"]
-                 [io.replikativ/incognito "0.2.2"]]
+                 [org.clojure/clojurescript "1.10.764" :scope "provided"]
+                 [io.replikativ/incognito "0.2.5"]]
   :source-paths ["src"]
   :plugins [[lein-cljsbuild "1.1.8"]]
 
@@ -13,7 +13,7 @@
   :aliases {"all" ["with-profile" "default:+1.8:+1.9"]}
   :profiles {:dev {:dependencies [[figwheel-sidecar "0.5.19"]
                                   [com.cemerick/piggieback "0.2.2"]
-                                  [criterium "0.4.5"]]
+                                  [criterium "0.4.6"]]
                    :figwheel {:nrepl-port 7888
                               :nrepl-middleware ["cider.nrepl/cider-middleware"
                                                  "cemerick.piggieback/wrap-cljs-repl"]}
@@ -45,6 +45,6 @@
      :compiler
      {:output-to "resources/test/compiled.js"
       :optimizations :whitespace
-    :pretty-print true}}]
-  :test-commands {"unit-tests" ["phantomjs" "resources/test/compiled.js"
-                                "resources/test/unit-test.html"]}})
+      :pretty-print true}}]
+   :test-commands {"unit-tests" ["phantomjs" "resources/test/compiled.js"
+                                 "resources/test/unit-test.html"]}})
